4846b9c6563fbe309cc0e513d2b21a683eb08fee
David <j.david.prado1716@gmail.com>
2023-02-22 22:13:26 -0500

    first deploy

--- src/components/LayOut/Layout.js
+++ src/components/LayOut/Layout.js
@@ -15,7 +15,7 @@ export const Layout = ({ children }) => {
         <title>David Prado Portafolio</title>
         <meta name='description' content='David Prado Portafolio' />
       </Head>
-      <div className='mt-[110px] md:mt-0 relative min-h-screen flex flex-col'>
+      <div className='relative min-h-screen flex flex-col'>
         <Header />
         <main className={`mt-[112px] md:mt-0 flex-grow h-screen ${montserrat.className}`}>
           {children}
